6cf828b826a073ce478fd01e539ce0ad970c939c
cjkriska <charleskriska@gmail.com>
2025-01-27 16:03:42 -0600

    initial commit - basic Spring Boot application

---
+++ src/main/java/com/charliekriska/metabolic_health_api/MetabolicHealthApiApplication.java
@@ -0,0 +1,13 @@
+package com.charliekriska.metabolic_health_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetabolicHealthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetabolicHealthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charliekriska/metabolic_health_api/MetabolicHealthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charliekriska.metabolic_health_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetabolicHealthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
